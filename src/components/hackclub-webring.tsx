6d4a3f58dbd319a12ec0b978784e2cd39ade808c
Hexaa <128217934+hexaaagon@users.noreply.github.com>
2025-02-27 16:20:53 +0000

    refactor(webring): Optimize Hack Club Webring by using Server Mutation. And fix several things.

--- src/components/hackclub-webring.tsx
+++ src/components/hackclub-webring.tsx
@@ -1,5 +1,7 @@
 "use client";
-import { useEffect, useState } from "react";
+import useSWR from "swr";
+import { getWebring } from "@/lib/actions/webring";
+
 import Link from "next/link";
 import Image from "next/image";
 
@@ -12,42 +14,22 @@ import {
 } from "@/components/ui/tooltip";
 
 export default function HCWebring() {
-  const [prevUrl, setPrevUrl] = useState<string>("#");
-  const [nextUrl, setNextUrl] = useState<string>("#");
-
-  useEffect(() => {
-    const fetchWebring = async () => {
-      try {
-        const res = await fetch("https://webring.hackclub.com/members.json");
-        const members: { url: string }[] = await res.json();
-
-        const currentHostname = window.location.hostname.toLowerCase();
-        const currentIndex = members.findIndex(
-          (member) => new URL(member.url).hostname === currentHostname,
-        );
-
-        if (currentIndex !== -1) {
-          const prevIndex =
-            (currentIndex - 1 + members.length) % members.length;
-          const nextIndex = (currentIndex + 1) % members.length;
-          setPrevUrl(members[prevIndex].url);
-          setNextUrl(members[nextIndex].url);
-        }
-      } catch (error) {
-        console.error("Failed to fetch webring members:", error);
-      }
-    };
-
-    fetchWebring();
-  }, []);
+  const { data } = useSWR(
+    "https://webring.hackclub.com/members.json",
+    getWebring,
+  );
 
   return (
     <TooltipProvider>
       <Tooltip delayDuration={50}>
         <TooltipTrigger className="group flex items-center space-x-2">
-          <Link href={prevUrl} id="previousBtn">
-            <ChevronLeft size={16} />
-          </Link>
+          {!data ? (
+            <></>
+          ) : (
+            <Link href={data.previous.url}>
+              <ChevronLeft size={16} />
+            </Link>
+          )}
           <Link href="https://webring.hackclub.com">
             <Image
               src="/static/images/hackclub.svg"
@@ -57,9 +39,13 @@ export default function HCWebring() {
               className="grayscale transition-[filter] duration-300 group-hover:grayscale-0"
             />
           </Link>
-          <Link href={nextUrl} id="nextBtn">
-            <ChevronRight size={16} />
-          </Link>
+          {!data ? (
+            <></>
+          ) : (
+            <Link href={data.next.url}>
+              <ChevronRight size={16} />
+            </Link>
+          )}
         </TooltipTrigger>
         <TooltipContent>
           <p>Click the arrows to see other websites of Hack Club members.</p>
