ac44356ee96fdac6571a4dd778a1399bbe1d3223
Hexaa <me@hexagonn.my.id>
2025-03-18 18:48:38 +0700

    fix(page): add temporary spacing in Landing component for layout adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,70 @@ export default function Landing() {
   return (
     <main>
       <BlurText text="heya. under construction." direction="bottom" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      just a temp.
     </main>
   );
 }
