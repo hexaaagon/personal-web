fa36031b1b4c7b783f9e99a729ab559c72c01755
Hexaa <128217934+hexaaagon@users.noreply.github.com>
2025-05-24 21:05:11 +0700

    just another redesign (#2)
    * feat: going back to zero.
    
    * feat: the day has come. enhance project structure and UI components
    
    - Updated package.json to include new dependencies: @radix-ui/react-tooltip, @react-spring/web, lenis, prettier-plugin-tailwindcss, and swr.
    - Modified globals.css to add new font variables and adjust color variables for better theming.
    - Refactored layout.tsx to integrate new fonts and wrap children with ThemeProvider for consistent theming.
    - Enhanced page.tsx to provide a more informative homepage with links to development and old website.
    - Added .prettierrc for prettier configuration with tailwindcss support.
    - Introduced hackclub.svg for branding purposes.
    - Created template.tsx to standardize layout with Navbar and Footer components.
    - Developed footer.tsx to include GitHub link and Hack Club Webring.
    - Implemented hackclub-webring.tsx to fetch and display Hack Club members with navigation.
    - Built navbar.tsx to include theme switch functionality.
    - Established theme-provider.tsx to manage theme context and toast notifications.
    - Created theme-switch.tsx for toggling between light and dark themes.
    - Developed skeleton.tsx for loading placeholders.
    - Implemented text.tsx for animated text effects.
    - Created tooltip.tsx for reusable tooltip components.
    - Added webring.ts to handle fetching and processing of Hack Club Webring data.
    
    * chore(favicon): update favicon.ico file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center *:transition-[font-size,line-height]">
-      <p className="text-3xl sm:text-5xl">Work in Progress!</p>
-      <p className="text-muted-foreground text-justify text-xs sm:text-base">
+    <main className="flex flex-col items-center justify-center">
+      <p className="text-5xl">Work in Progress!</p>
+      <p className="text-muted-foreground text-justify">
         yeah, this site is still under development.
       </p>
-      <div className="text-muted-foreground/50 flex gap-2 font-mono text-xs *:transition *:hover:underline sm:text-sm">
+      <div className="text-muted-foreground/50 flex gap-2 font-mono text-sm *:transition *:hover:underline">
         <Link href="https://redesign.hexaa.sh">[development]</Link>
         <Link href="https://hexagonn.my.id">[old website]</Link>
       </div>
