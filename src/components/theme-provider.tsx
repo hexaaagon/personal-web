26e3b875ff9b7a9dc042cac9bb0745f2e4b6f62b
Hexaa <me@hexagonn.my.id>
2025-05-25 11:19:40 +0700

    feat(vercel): add Vercel analytics and speed insights to the project

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,5 +1,8 @@
 import { type ComponentProps } from "react";
 
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 //import { ViewTransitions } from "next-view-transitions";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { Toaster } from "@/components/ui/sonner";
@@ -7,11 +10,16 @@ import ReactLenis from "lenis/react";
 
 export function ThemeProvider({ children }: ComponentProps<"div">) {
   return (
-    //  <ViewTransitions>
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
-      <ReactLenis root>{children}</ReactLenis>
-      <Toaster richColors expand />
-    </NextThemesProvider>
-    //  </ViewTransitions>
+    <>
+      {/*<ViewTransitions>*/}
+      <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+        <ReactLenis root>{children}</ReactLenis>
+        <Toaster richColors expand />
+      </NextThemesProvider>
+      {/*</ViewTransitions>*/}
+
+      <Analytics />
+      <SpeedInsights />
+    </>
   );
 }
